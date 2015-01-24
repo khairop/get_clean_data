9009cadba1430baece2dacea3e325f84e69f49a7
Kostas Chairopoulos <khairop@gmail.com>
2015-01-24 13:20:04 +0200

    fifth commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,85 +1,100 @@
-library(base)
-library(utils)
-library(data.table)
+library(reshape2)
 
-# The function downloads the Samsung data and extracts it
-download.data <- function () {
-  zip.url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-  zip.file <- 'dataset.zip'
+clean.file <- "cleaned.txt"
+extracted.features <- c(1, 2, 3, 4, 5, 6, 41, 42, 43, 44, 45, 46, 81, 82, 83, 84, 85, 86, 121, 122, 123, 124, 125, 126, 161, 162, 163, 164, 165, 166, 201, 202, 214, 215, 227, 228, 240, 241, 253, 254, 266, 267, 268, 269, 270, 271, 345, 346, 347, 348, 349, 350, 424, 425, 426, 427, 428, 429, 503, 504, 516, 517, 529, 530, 542, 543)
+extracted.feature.names <- c("tBodyAcc-mean()-X", "tBodyAcc-mean()-Y", "tBodyAcc-mean()-Z", "tBodyAcc-std()-X", "tBodyAcc-std()-Y", "tBodyAcc-std()-Z", "tGravityAcc-mean()-X", "tGravityAcc-mean()-Y", "tGravityAcc-mean()-Z", "tGravityAcc-std()-X", "tGravityAcc-std()-Y", "tGravityAcc-std()-Z", "tBodyAccJerk-mean()-X", "tBodyAccJerk-mean()-Y", "tBodyAccJerk-mean()-Z", "tBodyAccJerk-std()-X", "tBodyAccJerk-std()-Y", "tBodyAccJerk-std()-Z", "tBodyGyro-mean()-X", "tBodyGyro-mean()-Y", "tBodyGyro-mean()-Z", "tBodyGyro-std()-X", "tBodyGyro-std()-Y", "tBodyGyro-std()-Z", "tBodyGyroJerk-mean()-X", "tBodyGyroJerk-mean()-Y", "tBodyGyroJerk-mean()-Z", "tBodyGyroJerk-std()-X", "tBodyGyroJerk-std()-Y", "tBodyGyroJerk-std()-Z", "tBodyAccMag-mean()", "tBodyAccMag-std()", "tGravityAccMag-mean()", "tGravityAccMag-std()", "tBodyAccJerkMag-mean()", "tBodyAccJerkMag-std()", "tBodyGyroMag-mean()", "tBodyGyroMag-std()", "tBodyGyroJerkMag-mean()", "tBodyGyroJerkMag-std()", "fBodyAcc-mean()-X", "fBodyAcc-mean()-Y", "fBodyAcc-mean()-Z", "fBodyAcc-std()-X", "fBodyAcc-std()-Y", "fBodyAcc-std()-Z", "fBodyAccJerk-mean()-X", "fBodyAccJerk-mean()-Y", "fBodyAccJerk-mean()-Z", "fBodyAccJerk-std()-X", "fBodyAccJerk-std()-Y", "fBodyAccJerk-std()-Z", "fBodyGyro-mean()-X", "fBodyGyro-mean()-Y", "fBodyGyro-mean()-Z", "fBodyGyro-std()-X", "fBodyGyro-std()-Y", "fBodyGyro-std()-Z", "fBodyAccMag-mean()", "fBodyAccMag-std()", "fBodyBodyAccJerkMag-mean()", "fBodyBodyAccJerkMag-std()", "fBodyBodyGyroMag-mean()", "fBodyBodyGyroMag-std()", "fBodyBodyGyroJerkMag-mean()", "fBodyBodyGyroJerkMag-std()")
+activities <- c(1, 2, 3, 4, 5, 6)
+activity.names <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING")
 
-  download.file(zip.url, destfile = zip.file, method = 'curl')
-  unzip(zip.file)
+# A helper method for printing to the console.
+p <- function(...) {
+  cat("[run_analysis.R]", ..., "\n")
 }
 
-# The function loads and processes either a train or a test data set,
-# given that current directory is the Samsung data set.
-load.dataset <- function (set, features, labels) {
-  # Construct the relative pathes of data files
-  prefix <- paste(set, '/', sep = '')
-  file.data <- paste(prefix, 'X_', set, '.txt', sep = '')
-  file.label <- paste(prefix, 'y_', set, '.txt', sep = '')
-  file.subject <- paste(prefix, 'subject_', set, '.txt', sep = '')
-
-  # Cannot load the data using fread() function.
-  # fread() fails to determine the correct number of columns in dataset.
-  # So we read the data into a data.frame and then transform it into data.table
-  data <- read.table(file.data)[, features$index]
-  names(data) <- features$name
-
-  label.set <- read.table(file.label)[, 1]
-  data$label <- factor(label.set, levels=labels$level, labels=labels$label)
-
-  subject.set <- read.table(file.subject)[, 1]
-  data$subject <- factor(subject.set)
-
-  # convert to data table
-  data.table(data)
+# Makes a features filename given a dataset name.
+features.file <- function(name) {
+  paste("X_", name, ".txt", sep = "")
 }
 
-run.analysis <- function () {
-  setwd('UCI HAR Dataset/')
-
-  # Get the features
-  feature.set <- read.table('features.txt', col.names = c('index', 'name'))
-  features <- subset(feature.set, grepl('-(mean|std)[(]', feature.set$name))
-
-  # Get the labels
-  label.set <- read.table('activity_labels.txt', col.names = c('level', 'label'))
-
-  # Read train and test data sets
-  train.set <- load.dataset('train', features, label.set)
-  test.set <- load.dataset('test', features, label.set)
-
-  # The raw data set
-  dataset <- rbind(train.set, test.set)
-
-  # Generate the tidy data set
-  tidy.dataset <- dataset[, lapply(.SD, mean), by=list(label, subject)]
-  # Fix the variable names
-  names <- names(tidy.dataset)
-  names <- gsub('-mean', 'Mean', names) # Replace `-mean' by `Mean'
-  names <- gsub('-std', 'Std', names) # Replace `-std' by 'Std'
-  names <- gsub('[()-]', '', names) # Remove the parenthesis and dashes
-  names <- gsub('BodyBody', 'Body', names) # Replace `BodyBody' by `Body'
-  setnames(tidy.dataset, names)
-
-  # Write the raw and the tidy data sets to files
-  setwd('..')
-  write.csv(dataset, file = 'rawdata.csv', row.names = FALSE)
-  write.csv(tidy.dataset, file = 'tidydata.csv',
-	    row.names = FALSE, quote = FALSE)
-
-  # Return the tidy data set
-  tidy.dataset
+# Makes an activities filename given a dataset name.
+activities.file <- function(name) {
+  paste("Y_", name, ".txt", sep = "")
 }
 
+# Makes a subjects filename given a dataset name.
+subjects.file <- function(name) {
+  paste("subject_", name, ".txt", sep = "")
+}
 
+# Returns an interim dataframe for a single dataset.
+get.data <- function(dir, name) {
+  # Setup the file paths.
+  real.dir <- file.path(dir, name)
+  features.name <- file.path(real.dir, features.file(name))
+  activities.name <- file.path(real.dir, activities.file(name))
+  subjects.name <- file.path(real.dir, subjects.file(name))
 
-/***************************************/
-#echo # get_clean_data >> README.md
-#git init
-#git add README.md
-#git commit -m "first commit"
-#git remote add origin https://github.com/khairop/get_clean_data.git
-#git push -u origin master
+  p("Getting dataset:", real.dir)
 
+  # Read the features table.
+  p("  reading features...")
+  features.t <- read.table(features.name)[extracted.features]
+  names(features.t) <- extracted.feature.names
+
+  clean.data <- features.t
+
+  # Read the activities list.
+  p("  reading activities...")
+  activities.t <- read.table(activities.name)
+  names(activities.t) <- c("activity")
+  activities.t$activity <- factor(activities.t$activity, levels = activities, labels = activity.names)
+  clean.data <- cbind(clean.data, activity = activities.t$activity)
+
+  # Read the subjects list.
+  p("  reading subjects...")
+  subjects.t <- read.table(subjects.name)
+  names(subjects.t) <- c("subject")
+  clean.data <- cbind(clean.data, subject = subjects.t$subject)
+
+  # Return the clean data
+  clean.data
+}
+
+# Performs the full analysis of both the test and train
+# datasets. Writes a clean dataset to disk.
+run.analysis <- function(dir) {
+  p("Getting and Cleaning Data Project")
+  p("Author: William Bowers")
+  p("---")
+  p("Starting up.")
+  p("Preparing to run analysis.")
+
+  # Read the data.
+  p("Reading datasets.")
+  test <- get.data(dir, "test")
+  train <- get.data(dir, "train")
+
+  # Join the data.
+  p("Joining datasets.")
+  all.data <- rbind(test, train)
+
+  # Reshape the data.
+  p("Melting.")
+  all.data.long <- melt(all.data, id = c("subject", "activity"))
+  p("Dcasting.")
+  all.data.wide <- dcast(all.data.long, subject + activity ~ variable, mean)
+
+  # Set the clean data.
+  all.data.clean <- all.data.wide
+
+  # Save the clean data.
+  clean.file.name <- file.path(dir, clean.file)
+  p("Saving clean data to:", clean.file.name)
+  write.table(all.data.clean, clean.file.name, row.names = FALSE, quote = FALSE)
+}
+
+# Run the analysis.
+run.analysis("/repos/Getting-and-Cleaning-Data-Project/data")
+
+#ddddd
+#dddddd
